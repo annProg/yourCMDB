248a614e8a1f240748d9eb0e1543fab78173b33c
annprog <tecbbs@qq.com>
2015-12-23 19:25:48 +0800

    object field word-break

--- web/object/ShowObject.php
+++ web/object/ShowObject.php
@@ -243,7 +243,7 @@
 			$fieldType = $config->getObjectTypeConfig()->getFieldType($object->getType(), $field);
 			echo "<tr>";
 			echo "<td>$fieldLabel:</td>";
-			echo "<td>";
+			echo "<td style=\"word-break:break-all\">";
 			echo showFieldForDataType($object->getType(), $fieldName, $fieldValue, $fieldType, false);
 			echo "</td>";
 			echo "</tr>";
